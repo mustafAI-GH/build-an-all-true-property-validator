5763446bc3b6b7d5bbb18c196b802926f116331b
Mustafa <koc.mustafaa1@gmail.com>
2025-12-07 09:45:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function truthCheck(collection, pre) {
+
+  for (let i = 0; i < collection.length; i ++) {
+
+    if (!collection[i][pre]) {
+      return false;
+    }
+  }
+     return true;
+}
+
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"));
+
+console.log(truthCheck([{id: 1, data: {url: "https://freecodecamp.org", name: "freeCodeCamp"}}, {id: 2, data: {url: "https://coderadio.freecodecamp.org/", name: "CodeRadio"}}, {id: null, data: {}}], "data"))
